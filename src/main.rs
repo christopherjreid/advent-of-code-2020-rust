50a9bbf6eb673d6a6f37aad372f524c69628db9d
christopherjreid <58059133+christopherjreid@users.noreply.github.com>
2020-12-22 21:51:07 -0500

    Add Day 02a - Count valid passwords (#4)
    Count number of valid passwords in a file, provided the password policy.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ mod days_of_advent;
 
 fn main() {
     println!("Day 01: Repair Report");
-    println!("\tDay 01: Multiply n addends of 2020");
+    println!("\tMultiply n addends of 2020");
     days_of_advent::day01::run();
+
+    println!("Day 02: Password Philosophy");
+    println!("\tFind passwords that match the policy");
+    days_of_advent::day02::run();
 }
